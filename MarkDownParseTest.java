ff134dda201c0d15f6abd0c2ba81cedb7883ffe8
oXOjasviXo <ojasvi2003@gmail.com>
2022-04-21 08:52:04 -0700

    adding lib after gitignore edit

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -1,9 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkDownParseTest {
+    private static MarkdownParse test;
+
     @Test
     public void addition(){
         assertEquals(2, 1+1);
+    }
+    @Test
+    public void getLinksTest(){
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(expected,test.getLinks(content));
     }
 }
