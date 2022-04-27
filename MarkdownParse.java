2c2d7e877f5b2a2401174321dc63b51e0323c652
oXOjasviXo <ojasvi2003@gmail.com>
2022-04-26 20:36:03 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,9 +17,6 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             
-            if(openBracket == -1 || closeBracket == -1 || openParen == -1 ){
-                break;
-            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
