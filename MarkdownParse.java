c88d7ebb8db2714ab627c281253bd25bb46b3f52
oXOjasviXo <ojasvi2003@gmail.com>
2022-04-26 20:39:34 -0700

    Fix for error 2 (BreakMyComp.md) that caused the following output:
    Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
            at java.base/java.lang.StringLatin1.newString(StringLatin1.java:769)
            at java.base/java.lang.String.substring(String.java:2709)
            at MarkdownParse.getLinks(MarkdownParse.java:22)
            at MarkdownParse.main(MarkdownParse.java:33)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,7 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if( closeBracket == -1){
+            if(openBracket == -1 || closeBracket == -1 ){
                 break;
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
